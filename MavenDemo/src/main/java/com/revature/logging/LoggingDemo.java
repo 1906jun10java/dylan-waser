058ac147a0a5c9c2d0993b4b88e3be65f991cacd
Dylan Waser <waserd@etsu.edu>
2019-06-27 14:31:11 -0400

    fixed .gitignore

--- MavenDemo/src/main/java/com/revature/logging/LoggingDemo.java
+++ MavenDemo/src/main/java/com/revature/logging/LoggingDemo.java
@@ -4,11 +4,20 @@ import org.apache.logging.log4j.*;
 
 public class LoggingDemo {
 
-	private static Logger log = Logger.getRootLogger();
+	private static Logger log = LogManager.getLogger(LoggingDemo.class);
 	
 	public static void main(String[] args) 
 	{
-		
+		allTheLogs();
 	}
 
+	static void allTheLogs()
+	{
+		log.trace("trace message_");
+		log.debug("debug message");
+		log.info("info message");
+		log.warn("warn message");
+		log.error("error message");
+		log.fatal("fatal message");
+	}
 }
